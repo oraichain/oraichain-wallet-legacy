42b0de7d6c7201eec09b6c3f8ba6960ff6a93989
chuyenbka <chuyenbka@gmail.com>
2021-04-12 18:01:18 +0700

    connect from wallet scan

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -2,18 +2,38 @@ import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
 // import { useHistory } from 'react-router';
-import PinWrap, { openPinWrap } from './PinWrap';
+import queryString from 'query-string';
+import _ from 'lodash';
+import Big from 'big.js';
 
-const Transaction = ({ user }) => {
+import PinWrap, { openPinWrap } from './PinWrap';
+import { getTxBodySend, getTxBodyMultiSend, getTxBodyDelegate, getTxCreateValidator, getTxBodyUndelegate, getTxBodyMsgWithdrawDelegatorReward } from './utils';
+
+const Transaction = ({ user, history }) => {
   const $ = window.jQuery;
   const { t, i18n } = useTranslation();
-  // const history = useHistory();
-  // const cosmos = window.cosmos;
+  const queryStringParse = queryString.parse(history.location.search) || {};
+  const payload = JSON.parse(queryStringParse.raw_message || '{}');
+  const cosmos = window.cosmos;
 
   useEffect(() => {
     if (window.stdSignMsgByPayload) {
       const txBody = window.stdSignMsgByPayload;
       $('#tx-json').html(JSON.stringify(txBody));
+    } else if (payload && payload.value) {
+      const cloneObj = JSON.parse(JSON.stringify(payload));
+      if (_.get(cloneObj, "value.fee.amount") && cloneObj.value.fee.amount[0]) {
+        cloneObj.value.fee.amount[0] = new Big(cloneObj.value.fee.amount[0]).times(0.000001);
+      }
+      if(_.get(cloneObj, "value.msg.0.value.amount.0.amount")) {
+        const amountString = _.get(cloneObj, "value.msg.0.value.amount.0.amount");
+        _.set(cloneObj, "value.msg.0.value.amount.0.amount", new Big(amountString).times(0.000001))
+      }
+      if(_.get(cloneObj, "value.msg.0.value.amount.amount")) {
+        const amountString = _.get(cloneObj, "value.msg.0.value.amount.amount");
+        _.set(cloneObj, "value.msg.0.value.amount.amount", new Big(amountString).times(0.000001))
+      }
+      $('#tx-json').html(JSON.stringify(cloneObj.value));
     }
   }, []);
 
@@ -22,6 +42,60 @@ const Transaction = ({ user }) => {
     if (window.opener) {
       window.opener.postMessage('deny', '*');
       window.close();
+    }
+  };
+
+  const onChildKey = async (childKey) => {
+    try {
+      // will allow return childKey from Pin
+      const type = _.get(payload, 'type');
+      let txBody;
+      const memo = _.get(payload, 'value.memo') || "";
+      switch (type) {
+        case 'cosmos-sdk/MsgDelegate' : {
+          const amount = new Big(_.get(payload, 'value.msg.0.value.amount.amount') || 0).toString();
+          const validator_address = _.get(payload, 'value.msg.0.value.validator_address');
+          txBody = getTxBodyDelegate(user, validator_address, amount, memo);
+          break;
+        }
+        case 'cosmos-sdk/MsgUndelegate' : {
+          const amount = new Big(_.get(payload, 'value.msg.0.value.amount.amount') || 0).toString();
+          const validator_address = _.get(payload, 'value.msg.0.value.validator_address');
+          txBody = getTxBodyUndelegate(user, validator_address, amount, memo);
+          break;
+        }
+        case 'cosmos-sdk/MsgCreateValidator': {
+          txBody = getTxCreateValidator(_.get(payload, 'value.msg.0.value'));
+          break;
+        }
+        case 'cosmos-sdk/MsgWithdrawDelegationReward': {
+          txBody = getTxBodyMsgWithdrawDelegatorReward(user, _.get(payload, 'value.msg.0.value.validator_address'));
+          break;
+        }
+        default : {
+          const msgs = _.get(payload, 'value.msg');
+          if (msgs.length > 1) {
+            txBody = getTxBodyMultiSend(user, msgs, memo);
+          } else {
+            const amount = new Big(_.get(payload, 'value.msg.0.value.amount.0.amount') || 0).toString();
+            const to = _.get(payload, 'value.msg.0.value.to_address');
+            txBody = getTxBodySend(user, to, amount, memo);
+          }
+        }
+        
+      }
+      // higher gas limit
+      const res = await cosmos.submit(childKey, txBody, 'BROADCAST_MODE_BLOCK') || {};
+      if (queryStringParse.signInFromScan) {
+        window.opener.postMessage(res.tx_response, "*");
+        window.close();
+      } else {
+        $('#tx-json').text(res.tx_response.raw_log);
+      }
+    } catch (ex) {
+      alert(ex.message);
+    } finally {
+      // setBlocking(false);
     }
   };
 
@@ -42,7 +116,7 @@ const Transaction = ({ user }) => {
         </div>
       </form>
 
-      <PinWrap show={false} pinType="tx" />
+      <PinWrap show={false} pinType="tx" onChildKey={onChildKey} closePopup={queryStringParse.signInFromScan}/>
     </div>
   );
 };
