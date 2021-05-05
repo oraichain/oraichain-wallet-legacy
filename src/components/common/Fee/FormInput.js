cbe5619c94fd8570272547bb8fa4211d2b3735b5
DUONGTK-GD\DuongTK <duongtd@rikkeisoft.com>
2021-05-05 14:34:14 +0700

    Adding common form input controller

---
+++ src/components/common/Fee/FormInput.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {useFormContext, Controller} from "react-hook-form";
+import TextField from "@material-ui/core/TextField";
+
+import styles from "./FormInput.scss";
+
+const cx = cn.bind(styles);
+
+function FormInput(props) {
+	const {control} = useFormContext();
+	const {name, placeholder, label, required, errorobj, classNameCustom} = props;
+	let isError = false;
+	let errorMessage = "";
+	if (errorobj && errorobj.hasOwnProperty(name)) {
+		isError = true;
+		errorMessage = errorobj[name].message;
+	}
+
+	return (
+		<Controller
+			as={TextField}
+			name={name}
+			control={control}
+			placeholder={placeholder || ""}
+			label={label}
+			fullWidth
+			className={cx("input-text", classNameCustom)}
+			InputLabelProps={{
+				className: cx({"required-label": required}),
+				required: required || false,
+				shrink: true,
+			}}
+			error={isError}
+			helperText={errorMessage}
+			{...props}
+		/>
+	);
+}
+
+export default FormInput;
