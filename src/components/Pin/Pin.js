1b047a9313a133db7307d4568a481e52a1db5721
Pham The Huy <huypt@rikkeisoft.com>
2021-05-07 16:31:20 +0700

    Input pin

--- src/components/Pin/Pin.js
+++ src/components/Pin/Pin.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useState} from "react";
 import { useTranslation } from "react-i18next";
 import cn from "classnames/bind";
 import queryString from "query-string";
@@ -13,15 +13,64 @@ import PinWrap, { openPinWrap } from "src/components/PinWrap";
 import { getTxBodySend } from 'src/utils';
 import styles from "./Pin.module.scss";
 
+const mockCorrectPin = '0000';
 
 const message = Cosmos.message;
 const cx = cn.bind(styles);
 
-const SendTokens = () => {
+const Pin = () => {
     const { t, i18n } = useTranslation();
     const history = useHistory();
     const user = useSelector(state => state.user);
+    let [pinArray, setPinArray] = useState([]);
+    let [correctPin, setCorrectPin] = useState(false);
+
+    const handleClick = (e) => {
+      switch (e) {
+        case 'back':
+          pinArray.pop()
+          setPinArray([...pinArray])
+          break
+        case 'reset':
+          pinArray = []
+          setPinArray([])
+          break
+        default:
+          if (pinArray.length <= 4) {
+            pinArray.push(e)
+            setPinArray([...pinArray])
+          }
+      }
+      console.log(pinArray)
+
+      if (pinArray.length === 4)
+        evaluatePin(pinArray)
+    }
+
+    const evaluatePin = (pinArray) => {
+      const enteredPin = pinArray.join("");
+      if (enteredPin === mockCorrectPin) {
+        
+        setTimeout(() => {
+          openLock("correct PIN")
+          setTimeout(() => {
+            // setCorrectPin(true)
+          }, 500);
+        }, 250);
+        
+      } else {
+        setTimeout(() => {
+          openLock("wrong PIN")
+          setTimeout(() => {
+            // setCorrectPin(false)
+          }, 300);
+        }, 250);
+      }
+    }
     
+    const openLock = (result) => {
+      alert(result)
+    }
 
     return <div className={cx("pin-wrapper")}>
         <div className={cx("pin-display")}>
@@ -33,40 +82,41 @@ const SendTokens = () => {
           <div className={cx("confirmation-dots")}>
             <svg>
               <g>
-                <circle className={cx("pin-circle")} cx="10" cy="10" r="8"></circle>
-                <circle className={cx("pin-circle")} cx="50" cy="10" r="8"></circle>
-                <circle className={cx("pin-circle")} cx="90" cy="10" r="8"></circle>
+                <circle className={cx("pin-circle", pinArray.length > 0 ? "entered" : "")} cx="10" cy="10" r="8"></circle>
+                <circle className={cx("pin-circle", pinArray.length > 1 ? "entered" : "")} cx="40" cy="10" r="8"></circle>
+                <circle className={cx("pin-circle", pinArray.length > 2 ? "entered" : "")} cx="70" cy="10" r="8"></circle>
+                <circle className={cx("pin-circle", pinArray.length > 3 ? "entered" : "")} cx="100" cy="10" r="8"></circle>+
                 <circle className={cx("pin-circle")} cx="130" cy="10" r="8"></circle>
               </g>
             </svg>
           </div>
           <div className={cx("keypad")}>
             <div className={cx("keypad--row")}>
-              <div className={cx("keypad--button")} data-value="1">1</div>
-              <div className={cx("keypad--button")} data-value="2">2</div>
-              <div className={cx("keypad--button")} data-value="3">3</div>
+              <div className={cx("keypad--button")} onClick={() => handleClick(1)} disable="{disableClick}" >1</div>
+              <div className={cx("keypad--button")} onClick={() => handleClick(2)}>2</div>
+              <div className={cx("keypad--button")} onClick={() => handleClick(3)}>3</div>
             </div>
             <div className={cx("keypad--row")}>
-              <div className={cx("keypad--button")} data-value="4">4</div>
-              <div className={cx("keypad--button")} data-value="5">5</div>
-              <div className={cx("keypad--button")} data-value="6">6</div>
+              <div className={cx("keypad--button")} onClick={() => handleClick(4)}>4</div>
+              <div className={cx("keypad--button")} onClick={() => handleClick(5)}>5</div>
+              <div className={cx("keypad--button")} onClick={() => handleClick(6)}>6</div>
             </div>
             <div className={cx("keypad--row")}>
-              <div className={cx("keypad--button")} data-value="7">7</div>
-              <div className={cx("keypad--button")} data-value="8">8</div>
-              <div className={cx("keypad--button")} data-value="9">9</div>
+              <div className={cx("keypad--button")} onClick={() => handleClick(7)}>7</div>
+              <div className={cx("keypad--button")} onClick={() => handleClick(8)}>8</div>
+              <div className={cx("keypad--button")} onClick={() => handleClick(9)}>9</div>
             </div>
             <div className={cx("keypad--row")}>
-              <div className={cx("keypad--button")}><ArrowBack /></div>
-              <div className={cx("keypad--button")} data-value="0">0</div>
-              <div className={cx("keypad--button")}>x</div>
+              <div className={cx("keypad--button")} onClick={() => handleClick('back')}><ArrowBack /></div>
+              <div className={cx("keypad--button")} onClick={() => handleClick(0)}>0</div>
+              <div className={cx("keypad--button")} onClick={() => handleClick('reset')}>x</div>
             </div>
           </div>
         </div>
     </div>;
 };
 
-SendTokens.propTypes = {};
-SendTokens.defaultProps = {};
+Pin.propTypes = {};
+Pin.defaultProps = {};
 
-export default SendTokens;
+export default Pin;
