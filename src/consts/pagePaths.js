303683f5b5b4918c5e249f23fbe804cd2ae90153
thinhdt <thinhdt@rikkeisoft.com>
2021-05-27 17:13:15 +0700

    bugfix/open dialog when delegate

--- src/consts/pagePaths.js
+++ src/consts/pagePaths.js
@@ -2,7 +2,7 @@ export const pagePaths = {
     SIGNIN: "/signin",
     CREATE_WALLET: "/create-wallet",
     IMPORT_WALLET: "/import-wallet",
-    TX: "/tx",
+    TX: "/transaction",
     SEND_TOKENS: "/send-tokens",
     AI_REQUEST_SET: "/ai_request/set",
     HOME: "/",
