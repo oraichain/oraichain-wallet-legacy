d57898a9cbf08cd6c322dd12513b146417608c8c
thinhdt <thinhdt@rikkeisoft.com>
2021-06-03 13:44:52 +0700

    feature/show modal when click to suggestion

--- src/components/AuthLayout/AuthLayout.js
+++ src/components/AuthLayout/AuthLayout.js
@@ -1,23 +1,18 @@
 import React from "react";
-import {useHistory } from "react-router-dom";
 import cn from "classnames/bind";
 import PropTypes from "prop-types";
 import styles from "./AuthLayout.module.scss";
 import logoUrl from "src/images/logo.png";
-import { pagePaths } from "src/consts/pagePaths";
 
 const cx = cn.bind(styles);
 
 const AuthLayout = ({ children }) => {
-    const history = useHistory();
 
     return (
         <div className={cx("auth-layout")}>
             <div className={cx("auth-layout-header")}>
                 <div className="container d-flex flex-row justify-content-center align-items-center">
-                    <div className={cx("brand")} onClick={() => {
-                        history.push(pagePaths.HOME);
-                    }}>
+                    <div className={cx("brand")}>
                         <img className={cx("brand-icon")} src={logoUrl} alt="" />
                         <span className={cx("brand-text")}>Oraichain Wallet</span>
                     </div>
--- src/components/Suggestion/Suggestion.js
+++ src/components/Suggestion/Suggestion.js
@@ -1,25 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import cn from "classnames/bind";
 import PropTypes from "prop-types";
+import { Modal } from "react-bootstrap";
+import _ from "lodash";
 import QuestionIcon from "src/components/icons/QuestionIcon";
 import styles from "./Suggestion.module.scss";
 
 const cx = cn.bind(styles);
 
-const Suggestion = ({ text }) => {
-    return <div className={cx("suggestion")}>
-        <QuestionIcon className={cx("suggestion-icon")} />
-        <span className={cx("suggestion-text")}>
-            {text}
-        </span>
-    </div>;
+const Suggestion = ({ text, showModalWhenClick, modalTitle, modalBody }) => {
+    const [visible, setVisible] = useState(false);
+
+    const show = () => {
+        setVisible(true);
+    };
+
+    const hide = () => {
+        setVisible(false);
+    };
+
+    return (
+        <>
+            <div className={cx("suggestion")} onClick={() => {
+                if (showModalWhenClick) {
+                    show();
+                }
+
+            }}>
+                <QuestionIcon className={cx("suggestion-icon")} />
+                <span className={cx("suggestion-text")}>{text}</span>
+            </div>
+            <Modal show={visible} onHide={hide}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{modalTitle}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>{modalBody}</Modal.Body>
+            </Modal>
+        </>
+    );
 };
 
 Suggestion.propTypes = {
-    text: PropTypes.string
+    text: PropTypes.string,
+    showModalWhenClick: PropTypes.bool,
+    modalTitle: PropTypes.any,
+    modalBody: PropTypes.any,
 };
 Suggestion.defaultProps = {
-    text: ""
+    text: "",
+    showModalWhenClick: false,
+    modalTitle: "",
+    modalBody: "",
 };
 
 export default Suggestion;
