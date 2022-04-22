11d37dc93d2544d4244ca84290f3d6f02adf97af
Duc Pham Le <44611780+ducphamle2@users.noreply.github.com>
2022-04-22 14:18:55 +0700

    Develop (#51)
    * change to post message object tx
    
    Co-authored-by: haunv <haunv@haunvs-MacBook-Pro.local>

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,17 @@
 const constants = {
-    DATA_SOURCE: "datasource",
-    TEST_CASE: "testcase",
-    ORACLE_SCRIPT: "oscript"
-}
+  DATA_SOURCE: "datasource",
+  TEST_CASE: "testcase",
+  ORACLE_SCRIPT: "oscript",
+};
 
-export default constants;
+export const domainMessage = [
+  "https://scan.orai.io",
+  "https://datahub.orai.io",
+  "https://sso.orai.io",
+  "https://airight.io",
+  "https://staging.mainnet.airight.io",
+];
+
+export default {
+  constants,
+};
