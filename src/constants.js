926bbddf2d0055c6656ca5a694e7961e62cb376f
ducphamle2 <ducphamle212@gmail.com>
2022-06-29 09:10:10 +0700

    remove scan & prerelease scan from whitelist

--- src/constants.js
+++ src/constants.js
@@ -5,13 +5,11 @@ const constants = {
 };
 
 export const domainMessage = [
-  "https://scan.orai.io",
   "https://datahub.orai.io",
   "https://sso.orai.io",
   "https://airight.io",
   "https://staging.mainnet.airight.io",
   "https://staging.prerelease.airight.io",
-  "https://prerelease.scan.orai.io",
 ];
 
 export default {
